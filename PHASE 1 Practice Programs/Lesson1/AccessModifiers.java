5621a09ff7aad81dba258ca8cfcbf0ae60edccce
MogiliGnaneswari <97274229+MogiliGnaneswari@users.noreply.github.com>
2022-01-26 14:05:19 +0530

    Add files via upload
    Initial Commit

---
+++ PHASE 1 Practice Programs/Lesson1/AccessModifiers.java
@@ -0,0 +1,19 @@
+package com.gnaneswari.assignment;
+class defAccessSpecifier
+{ 
+  void display() 
+     { 
+         System.out.println("You are using defalut access specifier"); 
+     } 
+} 
+public class AccessModifiers {
+
+	public static void main(String[] args) {
+		System.out.println("Dafault Access Specifier");
+		defAccessSpecifier obj = new defAccessSpecifier(); 		  
+        obj.display(); 
+
+
+	}
+
+}
